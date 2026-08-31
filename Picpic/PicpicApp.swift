76aadb5c3c24b575d4bc728e4986ccbca13b8807
gabin <agencecwest@gmail.com>
2026-08-31 10:36:11 +0200

    Initial Commit

---
+++ Picpic/PicpicApp.swift
@@ -0,0 +1,17 @@
+//
+//  PicpicApp.swift
+//  Picpic
+//
+//  Created by PEELY on 31/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct PicpicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PicpicTests/PicpicTests.swift
@@ -0,0 +1,19 @@
+//
+//  PicpicTests.swift
+//  PicpicTests
+//
+//  Created by PEELY on 31/08/2026.
+//
+
+import Testing
+@testable import Picpic
+
+struct PicpicTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
